8d32f953a960ea32445ebf0b684b17d869fcc8ba
Viktor <vsirotkin15@gmail.com>
2021-09-20 13:48:47 +0300

    three tasks from lesson 1

---
+++ lesson1_task1.js
@@ -0,0 +1,2 @@
+let TempCelsius = prompt('Submit here a temperature in Celsius. The Machine turns it in Fahrenheit: ');
+alert(`${TempCelsius} degrees Celsius is equal ${(9 / 5) * TempCelsius + 32} Fahrenheit.`);
