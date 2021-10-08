ddc1ccc868347b864e207be2f1333891da701b88
Viktor <vsirotkin15@gmail.com>
2021-10-08 11:53:00 +0300

    tasks 1 thru 8 of lesson 2

---
+++ lesson2/lesson2_task7.js
@@ -0,0 +1,20 @@
+// Сравнить null и 0. Попробуйте объяснить результат.
+
+let a = 0;
+let b = null;
+
+result1 = a >= b;
+result2 = a <= b;
+result3 = a + b;
+result4 = a - b;
+result5 = a * b;
+result6 = a / b;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+
+//ответ:  null это отдельный вид данных и содержит только значение null. 0 же это уже конкретное значение.
